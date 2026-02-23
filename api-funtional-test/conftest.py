eac7f9f77c30902482a7cf419b2610f60033d1c7
ap6-dev <ap6.dev@gmail.com>
2026-02-23 12:48:22 -0600

    all positive api tests

--- api-funtional-test/conftest.py
+++ api-funtional-test/conftest.py
@@ -2,9 +2,9 @@ import logging
 import pytest
 from src.clients.api_client import APIClient
 
-#--------------------------
+#-------------------------------------------------------------------------------
 # Logging Setup
-#--------------------------
+#-------------------------------------------------------------------------------
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -15,7 +15,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+#-------------------------------------------------------------------------------
+# Pytest Fixture Setup
+#-------------------------------------------------------------------------------
 BASE_URL = "https://jsonplaceholder.typicode.com"
+#Json placeholder for testing
 
 @pytest.fixture
 def api_client():
--- api-funtional-test/tests/test_users.py
+++ api-funtional-test/tests/test_users.py
@@ -7,9 +7,11 @@ with open("data/users_test_data.json") as f:
     users_test_data = json.load(f)
 
 
-
+#-------------------------------------------------------------------------------
+# Positive GET /users Tests
+#-------------------------------------------------------------------------------
 #Test to verify that list of users is not empty
-def test_get_all_users(api_client):
+def test_get_all_users_returns_200(api_client):
     #Act
     response = api_client.get("/users")
 
@@ -19,11 +21,48 @@ def test_get_all_users(api_client):
     assert isinstance(data, list)
     assert len(data) > 0
 
+#Test that all uses for proper schema
+def test_get_all_users_schema(api_client):
+    #Act
+    response = api_client.get("/users")
 
+    #Assert
+    assert response.status_code == 200
+    data = response.json()
+    #Reusable validation helper
+    validate_user_fields(data)
+
+#-------------------------------------------------------------------------------
+# Positive GET /users/{id} Tests
+#-------------------------------------------------------------------------------
+#Test specified user fields exist
+@pytest.mark.parametrize("user_id", [1])
+def test_get_user_fields_present(api_client, user_id):
+    #Act
+    response = api_client.get(f"/users/{user_id}")
+
+    #Assert
+    assert response.status_code == 200
+    data = response.json()
+    #Helper function to validate all fields
+    validate_user_fields(data)
+
+
+#Test specified user(s) id exists
+@pytest.mark.parametrize("user_id", [1, 2, 3])
+def test_get_user_valid_id(api_client, user_id):
+    #Act
+    response = api_client.get(f"/users/{user_id}")
+
+    #Assert
+    assert response.status_code == 200
+    data = response.json()
+    #Verify returned id matches the requested id
+    assert data["id"] == user_id
 
 #Test for users specified in users_test_data
 @pytest.mark.parametrize("user_data", users_test_data)
-def test_get_user_parametrized(api_client, user_data):
+def test_get_user_data_matches_expected(api_client, user_data):
     #Arrange
     user_id = user_data["id"]
     
@@ -36,41 +75,13 @@ def test_get_user_parametrized(api_client, user_data):
     assert data["name"] == user_data["name"]
     assert data["email"] == user_data["email"]
 
-
-
-#Test that specified user data is valid
-@pytest.mark.parametrize("user_id", [1, 2, 3])
-def test_get_user_valid_id(api_client, user_id):
-    #Act
-    response = api_client.get(f"/users/{user_id}")
-
-    #Assert
-    assert response.status_code == 200
-    data = response.json()
-
-    #Reusable validation helper
-    validate_user_fields(data)
-
-    #Verify returned id matches the requested id
-    assert data["id"] == user_id
-
-
-
-#Negative test for specified invalid user id
-@pytest.mark.parametrize("invalid_id", [0, -1, 999])
-def test_get_user_invalid(api_client, invalid_id):
-    #Act
-    response = api_client.get(f"/users/{invalid_id}")
-    
-    #Assert
-    assert response.status_code == 404
-
-
-
-#Test creating a user
+#-------------------------------------------------------------------------------
+# Positive POST Tests
+#-------------------------------------------------------------------------------
+#Test create user
 #Using JSONPlaceholder as endpoint
 @pytest.mark.parametrize("user_payload", users_test_data)
-def test_create_user(api_client, user_payload):
+def test_create_user_valid_payload(api_client, user_payload):
     #Act
     response = api_client.post("/users", json=user_payload)
     data = response.json()
@@ -84,4 +95,57 @@ def test_create_user(api_client, user_payload):
     #Only validate the fields that are reliable
     for key in ("name", "email"):
         assert data[key] == user_payload[key]
+
+#Test create user minimum payload------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Positive PUT Tests
+#-------------------------------------------------------------------------------
+#Test updated fields are returned
+@pytest.mark.parametrize("user_payload", users_test_data)
+def test_update_user_valid_payload(api_client, user_payload):
+    #Act
+    response = api_client.put(api_client, user_payload)
+
+    #Assert
+    assert response.status_code == 200
+    data = response.json()
     
+    #Check that update fields are returned
+    #Helper function to validate all fields
+    validate_user_fields(data)
+
+#-------------------------------------------------------------------------------
+# Positive DELETE Tests
+#-------------------------------------------------------------------------------
+#Test valid return code for user deletion
+@pytest.mark.parametrize("user_payload", users_test_data)
+def test_delete_user_valid_id(api_client, user_payload):
+    #Act
+    response = api_client.delete(api_client, user_payload)
+
+    #Assert
+    assert response.status_code == 200 or 204
+
+
+
+#-------------------------------------------------------------------------------
+# Negative GET Tests
+#-------------------------------------------------------------------------------
+#Test for specified invalid user id
+@pytest.mark.parametrize("invalid_id", [0, -1, 999])
+def test_get_user_invalid_id_returns_404(api_client, invalid_id):
+    #Act
+    response = api_client.get(f"/users/{invalid_id}")
+    
+    #Assert
+    assert response.status_code == 404
+
+#Test get user by string - expected 404
+@pytest.mark.parametrize("invalid_id", "abc")
+def test_get_user_string_id(api_client, invalid_id):
+    #Act
+    response = api_client.get(f"/users/{invalid_id}")
+
+    #Assert
+    assert response.status_code == 404
