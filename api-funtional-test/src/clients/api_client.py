0dc130f579da973e6e8cce771efaf4520e5bcd67
apoullie6 <apoullie6@gmail.com>
2026-02-24 17:55:40 -0600

    added performance metrics to log file

--- api-funtional-test/src/clients/api_client.py
+++ api-funtional-test/src/clients/api_client.py
@@ -1,7 +1,18 @@
 import requests
 import logging
+import time
 
 logger = logging.getLogger(__name__)
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        duration = end - start
+        logging.info(f"[PERF] {func.__name__} took {duration*1000:.1f} ms\n")
+        return result
+    return wrapper
 
 #--------------------------
 # API Client
@@ -19,18 +30,22 @@ class APIClient:
         
         return self.session.request(method, full_url, **kwargs)
     
+    @measure_time
     def get(self, endpoint, **kwargs):
         logger.info(f"GET {self.base_url + endpoint}")
         return self._request("GET", endpoint, **kwargs)
     
+    @measure_time
     def post(self, endpoint, **kwargs):
         logger.info(f"POST {self.base_url + endpoint}")
         return self._request("POST", endpoint, **kwargs)
 
+    @measure_time
     def put(self, endpoint, **kwargs):
         logger.info(f"PUT {self.base_url + endpoint}")
         return self._request("PUT", endpoint, **kwargs)
 
+    @measure_time
     def delete(self, endpoint, **kwargs):
         logger.info(f"DELETE {self.base_url + endpoint}")
         return self._request("DELETE", endpoint, **kwargs)
